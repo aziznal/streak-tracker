4cea4e380a2f90279bf844e0b815e71cd851a39a
aziznal <aziznal.dev@gmail.com>
2025-08-24 18:55:51 +0300

    add eslint

---
+++ eslint.config.ts
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  reactHooks.configs["recommended-latest"],
+]);
