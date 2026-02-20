b14867cf172789fad4d2ad847e16a5bae1d589c3
aziznal <aziznal.dev@gmail.com>
2026-02-20 23:39:37 +0300

    update tanstack

--- src/router.tsx
+++ src/router.tsx
@@ -1,20 +1,18 @@
-import { createRouter as createTanstackRouter } from '@tanstack/react-router'
+import { createRouter } from '@tanstack/react-router'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
 
 // Create a new router instance
-export const createRouter = () => {
-  return createTanstackRouter({
+export const getRouter = () => {
+  const router = createRouter({
     routeTree,
+    context: {},
+
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
   })
+
+  return router
 }
 
-// Register the router instance for type safety
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: ReturnType<typeof createRouter>
-  }
-}
